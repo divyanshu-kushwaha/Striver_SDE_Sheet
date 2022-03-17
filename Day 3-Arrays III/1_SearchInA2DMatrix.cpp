e8332f6896723230c73fbf1cd646b9b7ec223459
Divyanshu Kushwaha <divyanshukushwaha654@gmail.com>
2022-03-17 15:10:09 +0530

    solution

--- Day 3-Arrays III/1_SearchInA2DMatrix.cpp
+++ Day 3-Arrays III/1_SearchInA2DMatrix.cpp
@@ -1,5 +1,32 @@
 // https://leetcode.com/problems/search-a-2d-matrix/
 
+// Method 1
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int l = 0, h = m*n - 1;
+        while(l<=h){
+            int mid = (l+h)/2;
+            int row = mid/m;
+            int col = mid%m;
+            if(matrix[row][col]==target){
+                return true;
+            }
+            else if(matrix[row][col]>target){
+                h = mid - 1;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+        return false;
+    }
+};
+
+
+// Method 2
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
